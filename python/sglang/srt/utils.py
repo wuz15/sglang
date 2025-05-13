34d2d4b664c63e7840f167d021681cea27dc6986
DiweiSun <105627594+DiweiSun@users.noreply.github.com>
2025-05-13 13:24:47 +0800

    fix triton incompatibility issue

--- python/sglang/srt/utils.py
+++ python/sglang/srt/utils.py
@@ -78,12 +78,7 @@ from torch.func import functional_call
 from torch.library import Library
 from torch.profiler import ProfilerActivity, profile, record_function
 from torch.utils._contextlib import _DecoratorContextManager
-from triton.runtime.cache import (
-    FileCacheManager,
-    default_cache_dir,
-    default_dump_dir,
-    default_override_dir,
-)
+from triton.runtime.cache import FileCacheManager
 
 logger = logging.getLogger(__name__)
 
@@ -805,17 +800,17 @@ class CustomCacheManager(FileCacheManager):
         self.key = key
         self.lock_path = None
         if dump:
-            self.cache_dir = default_dump_dir()
+            self.cache_dir = triton.knobs.cache.dump_dir
             self.cache_dir = os.path.join(self.cache_dir, self.key)
             self.lock_path = os.path.join(self.cache_dir, "lock")
             os.makedirs(self.cache_dir, exist_ok=True)
         elif override:
-            self.cache_dir = default_override_dir()
+            self.cache_dir = triton.knobs.cache.override_dir
             self.cache_dir = os.path.join(self.cache_dir, self.key)
         else:
             # create cache directory if it doesn't exist
             self.cache_dir = (
-                os.getenv("TRITON_CACHE_DIR", "").strip() or default_cache_dir()
+                os.getenv("TRITON_CACHE_DIR", "").strip() or triton.knobs.cache.dir
             )
             if self.cache_dir:
                 self.cache_dir = f"{self.cache_dir}_{os.getpid()}"
